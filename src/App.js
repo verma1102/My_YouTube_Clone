6b470faa9b1cf7b8c5d29b05e99b101dc17db290
verma1102 <vermapiyush1102@gmail.com>
2022-02-05 17:51:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import RecommendedVideos from './RecommendedVideos';
+import SearchPage from './SearchPage';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/search/:searchTerm" element={
+            <><div className='app__page'>
+              <Sidebar />
+              <SearchPage />
+            </div></>
+          } />
+          <Route path="/" element={
+            <><div className='app__page'>
+              <Sidebar />
+              <RecommendedVideos />
+            </div></>
+          } />
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
+export default App;
---
+++ src/RecommendedVideos.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import './RecommendedVideos.css';
+import VideoCard from './VideoCard';
+
+function RecommendedVideos() {
+    return (
+        <div className='recommendedVideos'>
+            <h2>Recommended</h2>
+            <div className='recommendedVideos__videos'>
+                <VideoCard
+                    title="Dhindora | Official Music Video | BB Ki Vines"
+                    views="25M Views"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/l_ZIXrVEQcHTBTsmpt2CFiWJF9_0hwB3rngr1_lxozZ3Lz58Ij5TcDFOp2TYlioU2gI9RlyExw=s48-c-k-c0x00ffffff-no-rj"
+                    channel="BB Ki Vines"
+                    image="https://i.ytimg.com/vi/l8zlKap1JEo/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBImh5boPpx3OiRB9FW7PvbaWMxuQ"
+                />
+                <VideoCard
+                    title="Daaru With Dad | Harsh Beniwal"
+                    views="33M Views"
+                    timestamp="1 year ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLQ_r8ZRXW8qpx2F3WpUxBBnNP_u53_v0b5Fq9Qfmw=s48-c-k-c0x00ffffff-no-rj"
+                    channel="Harsh Beniwal"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAk11FQ7Qjm2QPSYVsB6qFEn-zEQQ7wMavpQ&usqp=CAU"
+                />
+                <VideoCard
+                    title="Mission Valentine | Ashish Chanchlani"
+                    views="38M Viwes"
+                    timestamp="11 months ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLTpCi8ciENdMQMyB2K42liwB9Z0ZzbRCRYE-NjdCQ=s88-c-k-c0x00ffffff-no-rj"
+                    channel="ashish chanchlani vines"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuZe8LNdR8yXdUqHERsun-ZgvP21Y55XZWiw&usqp=CAU"
+                />
+                <VideoCard
+                    title="Marvel Studios' Doctor Strange in the Multiverse of Madness | Official Teaser"
+                    views="33M Views"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/fGvQjp1vAT1R4bAKTFLaSbdsfdYFDwAzVjeRVQeikH22bvHWsGULZdwIkpZXktcXZc5gFJuA3w=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Marvel Entertainment"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0-si1S4WkE4TAhVbkbQTWtZ7NBwi-6F7AOA&usqp=CAU"
+                />
+                <VideoCard
+                    title="Roommate - Stand Up Comedy Ft. Anubhav Singh Bassi"
+                    views="53M Views"
+                    timestamp="9 months ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLS7IEPwzVpf3MOKIBHsLlIIG_UTH7CKO2iKYNxw4A=s48-c-k-c0x00ffffff-no-rj"
+                    channel="Anubhav Singh Bassi"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkFshFY6uz8GPPFrkxS47GbxqfM0C_O_F0NQ&usqp=CAU"
+                />
+                <VideoCard
+                    title="Doobey - Official Video | Gehraiyaan"
+                    views="16M Views"
+                    timestamp="7 day ago"
+                    channelImage="https://yt3.ggpht.com/vJEsALWnQIwPZ2oaVbFWPu1n4L0CDkHpAdGJ-KBPFsxM9hsMsvExFK1mnCGUrMzfC67XPVThmQ=s48-c-k-c0x00ffffff-no-rj"
+                    channel="Sony Music India"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ85AZbKIR4H5Z0D7mrVqY_LabXPEOdAc1IlQ&usqp=CAU"
+                />
+                <VideoCard
+                    title="Srivalli (Video) | Pushpa | Allu Arjun, Rashmika Mandanna"
+                    views="101M Views"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/DzaZaTX6gdgjjPI_vkNc2dPbI794UroI9fTAunua0fa7lukDj5NDkjfhS5-w2KXuvXS02r92=s48-c-k-c0x00ffffff-no-rj"
+                    channel="T-Series"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxbFkMXlT8ARb_cjFpL8TlZqgmPkHuczk81A&usqp=CAU"
+                />
+                <VideoCard
+                    title="Leh Ladakh ROADTRIP started with a Twist !!"
+                    views="1M Views"
+                    timestamp="7 months ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLR6LxypyH_3zuo6-8idTB2pNYZ9kUmL8DekvQHbKA=s88-c-k-c0x00ffffff-no-rj"
+                    channel="JS Films"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgKtrnR4jKp2gSlhoJcXBJ5tfhGr002m2GOg&usqp=CAU"
+                />
+                <VideoCard
+                    title="The Luxury Trip of Abu Dhabi"
+                    views="3M Views"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLQKjmBnL5LN65Ci8yqHEb9FwhNkyNbxz5PIZ_aHYQ=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Flying Beast "
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk8Ool8iRRn2m5FdvkwjyTPVTgFWdhkt6Yfw&usqp=CAU"
+                />
+                <VideoCard
+                    title="Jaan Ban Gaye - Full Video | Khuda Haafiz"
+                    views="9.8M Views"
+                    timestamp="1 year ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLQqRcxPdBWjWCN8FJYzl4FSrh4hHanWNy8pqwGW_w=s48-c-k-c0x00ffffff-no-rj"
+                    channel="Zee Music Company"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-kkVaHGTMCFl6_uRNECAXPGbxNz0paa8bSA&usqp=CAU"
+                />
+                <VideoCard
+                    title="Inside The Burj Al Arab (Dubai's 7 Star Hotel!)"
+                    views="3.2M Viwes"
+                    timestamp="2 weekss ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRhKMe_O-jsSc-cQEC-q1OGgUYphaJjVgSQMAcEvds=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Khalid Al Ameri"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoxaXTi1OmK-a71BuGM5239024P0Z9cLExew&usqp=CAU"
+                />
+                <VideoCard
+                    title="THARA BHAiiiii | CARRYMINATI"
+                    views="26M Views"
+                    timestamp="6 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLQbUd-BnMU4KZr-jDbk1x1edI7tlMq8SEyl69-j0Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="CarryMinati"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE4Luq52TLTrsJuYk5WVMfMmtNJRSv3X-QHShy9hzFBIQQHPqqj7zIYeKBAsk6oE3ASqg&usqp=CAU"
+                />
+                <VideoCard
+                    title="IAS , UPSC , CSE Topper Interview | Akshat Jain , AIR - 2"
+                    views="12M Views"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/apeuAKvlro33fAq1yLZJPbd_-nm12MN-Rbewyqt5W9keo2xm-TUdKwlShj2XUuBpLFhZwYcl=s88-c-k-c0x00ffffff-no-rj"
+                    channel="UNTOLD SCOPE"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5-UflxtWQGbbOAv72RauJhX2Vg5rErbSoPA&usqp=CAU"
+                />
+                <VideoCard
+                    title="Glimpse of LIGER | Vijay Deverakonda"
+                    views="22M Views"
+                    timestamp="3 weeks ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLQ7GkbJMx3cRmxUQyO4UNpmukpq2ptdBMX5J1ogoIU=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Dharma Productions"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrGS05Cst8K4ofN48ZrgdI9mA9aFEGuLmmbw&usqp=CAU"
+                />
+                <VideoCard
+                    title="All of Us Are Dead | Official Trailer | Netflix India"
+                    views="1M Views"
+                    timestamp="1 week ago"
+                    channelImage="https://yt3.ggpht.com/zgMN9BuSQByG1SrpmLwcNB3MQhjDhS_pl9H1h7TaRievMfS4UpU7Z36j77z5_hnIW4N8uFX3NA=s48-c-k-c0x00ffffff-no-rj"
+                    channel="Netflix India"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQowc_jSS5GODM9neFK2GwPfULItWhKbcZJng&usqp=CAU"
+                />
+                <VideoCard
+                    title="Gifted this DUCATI to Myself | REWIND 2021"
+                    views="750K Views"
+                    timestamp="3 weeks ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLR6LxypyH_3zuo6-8idTB2pNYZ9kUmL8DekvQHbKA=s88-c-k-c0x00ffffff-no-rj"
+                    channel="JS Films"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBSmELnfpRn9kdpNuwHaup25EhbbOgqTsPWQ&usqp=CAU"
+                />
+            </div>
+        </div>
+    );
+}
+export default RecommendedVideos;
